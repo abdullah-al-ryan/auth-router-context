d3fc35141a73c1cb382638b20d36d11de565b87f
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-05-19 12:29:35 +0600

    login handled

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
     const handleSubmit = event => {
         event.preventDefault();
+        
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCg_aPc-XpA-FOa64KYAIpWclbAp3i0N8U",
+  authDomain: "auth-router-context-4e66c.firebaseapp.com",
+  projectId: "auth-router-context-4e66c",
+  storageBucket: "auth-router-context-4e66c.appspot.com",
+  messagingSenderId: "1078095563070",
+  appId: "1:1078095563070:web:dca8d406efe1a2be48046f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
